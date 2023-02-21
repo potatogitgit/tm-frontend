0aca160339952217ea49e5f879041f2357c1ba8e
264064 <bobby.aquino@telusinternational.com>
2023-02-21 19:46:57 +0800

    Second commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import MaterialModal from './components/MaterialModal/MaterialModal';
-import LockScreen from 'react-lock-screen'
 import io from 'socket.io-client'
 
 <script src="http:localhost:3000/socket.io/socket.io.js" />
@@ -22,42 +21,32 @@ function App() {
     setShow(false)
   }
 
-  const getLockScreenUi = (setLock) => {
-    return (
-      <div style={{
-        width: '300px',
-        position: 'fixed',
-        border: '4px solid #333',
-        background: '#fff',
-        textAlign: 'center',
-        padding: '16px',
-        borderRadius: '4px',
-        top: 'calc(30% - 152px / 2)',
-        left: 'calc(49% - 300px / 2)'
-      }}>
-
-        <button className='btn btn-primary mx-2' onClick={() => setShow(true)}>Call HR</button>
-        <br></br>
-        <br></br>
-        <img
-          width="32"
-          src="https://cdn3.iconfinder.com/data/icons/wpzoom-developer-icon-set/500/102-256.png"
-          alt="lock"
-        />
-        <p style={{ color: 'blue' }}>Hi {test}</p>
-        <button onClick={() => setLock(false)}>unlock</button>
-      </div>
-    )
-  }
-
 
   return (
     <div className="App">
       <header className="App-header">
+        <div style={{
+          width: '300px',
+          position: 'fixed',
+          border: '4px solid #333',
+          background: '#fff',
+          textAlign: 'center',
+          padding: '16px',
+          borderRadius: '4px',
+          top: 'calc(30% - 152px / 2)',
+          left: 'calc(49% - 300px / 2)'
+        }}>
 
-        <LockScreen timeout={5000} ui={getLockScreenUi}>
-          <img src={logo} className="App-logo" alt="logo" />
-        </LockScreen>
+          <button className='btn btn-primary mx-2' onClick={() => setShow(true)}>Call HR</button>
+          <br></br>
+          <br></br>
+          <img
+            width="32"
+            src="https://cdn3.iconfinder.com/data/icons/wpzoom-developer-icon-set/500/102-256.png"
+            alt="lock"
+          />
+          <p style={{ color: 'blue' }}>Hi {test}</p>
+        </div>
 
       </header>
       <MaterialModal show={show} handleModalClose={handleModalClose} />
