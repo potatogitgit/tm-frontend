32df0bdda6487a1845867fd35d9773bafbec825c
264064 <bobby.aquino@telusinternational.com>
2023-02-21 22:10:36 +0800

    tesgs

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import MaterialModal from './components/MaterialModal/MaterialModal';
 import io from 'socket.io-client'
 
-<script src="https://tm-frontend-eight.vercel.app/socket.io/socket.io.js" />
+<script src="https://6a68-130-105-185-166.ap.ngrok.io/socket.io/socket.io.js" />
 const socket = io.connect("https://tm-frontend-eight.vercel.app")
 
 
